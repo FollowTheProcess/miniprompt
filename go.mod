f0f16de925f2c253537943cfc18d49d59cac205a
Tom Fleet <tomfleet2018@gmail.com>
2024-11-08 10:48:49 +0000

    Add the CLI

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/FollowTheProcess/miniprompt
 
 go 1.23
+
+require (
+	github.com/FollowTheProcess/cli v0.8.1
+	github.com/FollowTheProcess/msg v1.2.0
+)
--- main.go
+++ main.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/FollowTheProcess/cli"
+	"github.com/FollowTheProcess/msg"
+)
+
+var (
+	version   = "dev" // miniprompt's version number, set at compile time
+	commit    = ""    // The SHA1 of the commit miniprompt was built from, set at compile time
+	buildDate = ""    // The build timestamp, set at compile time
 )
 
 func main() {
-	fmt.Println("Design the architecture, name the components, document the details.")
+	if err := run(); err != nil {
+		msg.Error("%s", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	prompt := func() (*cli.Command, error) {
+		return cli.New(
+			"prompt",
+			cli.Short("Show the prompt"),
+			cli.Run(func(cmd *cli.Command, args []string) error {
+				fmt.Println("show the prompt here")
+				return nil
+			}),
+		)
+	}
+
+	cmd, err := cli.New(
+		"miniprompt",
+		cli.Short("A teeny tiny shell prompt"),
+		cli.Version(version),
+		cli.Commit(commit),
+		cli.BuildDate(buildDate),
+		cli.Allow(cli.NoArgs()),
+		cli.SubCommands(prompt),
+	)
+	if err != nil {
+		return fmt.Errorf("could not build miniprompt CLI: %w", err)
+	}
+
+	return cmd.Execute()
 }
