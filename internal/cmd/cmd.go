81a84269133fdb180fd718cbf3731be6dcca7a60
Tom Fleet <tomfleet2018@gmail.com>
2024-11-08 13:12:47 +0000

    Add a CWD module (#2)
    ## Summary
    
    <!-- Describe your changes in detail here, if it closes an open issue,
    include "Closes #<issue>" -->

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -5,8 +5,10 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
 
 	"github.com/FollowTheProcess/cli"
+	"github.com/FollowTheProcess/miniprompt/internal/module"
 )
 
 var (
@@ -15,14 +17,46 @@ var (
 	buildDate = ""    // The build timestamp, set at compile time
 )
 
+// ExecutionContext is the context in which miniprompt is being run, it includes
+// things like input/output streams, CWD etc.
+type ExecutionContext struct {
+	CWD    string    // The current working directory
+	Home   string    // The user's home directory
+	Stdout io.Writer // Writer for the prompt
+	Stderr io.Writer // Logging or error output
+	Args   []string  // Command line args
+}
+
+// Context returns the ExecutionContext miniprompt is being run in, taking
+// values from the operating system.
+func Context() (ExecutionContext, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return ExecutionContext{}, fmt.Errorf("could not get $HOME: %w", err)
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return ExecutionContext{}, fmt.Errorf("could not get $CWD: %w", err)
+	}
+
+	return ExecutionContext{
+		CWD:    cwd,
+		Home:   home,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+		Args:   os.Args[1:], // Trim the program off (first arg)
+	}, nil
+}
+
 // Build returns the miniprompt CLI.
-func Build(ctx context.Context, stdout, stderr io.Writer, args []string) (*cli.Command, error) {
+func Build(ctx context.Context, ex ExecutionContext) (*cli.Command, error) {
 	prompt := func() (*cli.Command, error) {
 		return cli.New(
 			"prompt",
 			cli.Short("Show the prompt"),
 			cli.Run(func(cmd *cli.Command, args []string) error {
-				fmt.Fprintln(cmd.Stdout(), "prompt showy showy")
+				fmt.Fprintf(cmd.Stdout(), "\n%s\n%s", module.CWD(ex.Home, ex.CWD), module.Symbol())
 				return nil
 			}),
 		)
@@ -34,9 +68,9 @@ func Build(ctx context.Context, stdout, stderr io.Writer, args []string) (*cli.C
 		cli.Commit(commit),
 		cli.BuildDate(buildDate),
 		cli.Allow(cli.NoArgs()),
-		cli.Stdout(stdout),
-		cli.Stderr(stderr),
+		cli.Stdout(ex.Stdout),
+		cli.Stderr(ex.Stderr),
 		cli.SubCommands(prompt),
-		cli.OverrideArgs(args),
+		cli.OverrideArgs(ex.Args),
 	)
 }
--- internal/cmd/cmd_test.go
+++ internal/cmd/cmd_test.go
@@ -3,23 +3,30 @@ package cmd_test
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/FollowTheProcess/miniprompt/internal/cmd"
+	"github.com/FollowTheProcess/miniprompt/internal/module"
 	"github.com/FollowTheProcess/test"
 )
 
 func TestSmoke(t *testing.T) {
 	stdout := &bytes.Buffer{}
 	stderr := &bytes.Buffer{}
+	testContext := cmd.ExecutionContext{
+		CWD:    "/somewhere/user/project",
+		Home:   "/somewhere/user",
+		Stdout: stdout,
+		Stderr: stderr,
+		Args:   []string{"prompt"},
+	}
 
-	args := []string{"prompt"}
-
-	cmd, err := cmd.Build(context.Background(), stdout, stderr, args)
+	cmd, err := cmd.Build(context.Background(), testContext)
 	test.Ok(t, err) // Command failed to build
 
 	err = cmd.Execute()
 	test.Ok(t, err) // Command should not error
 
-	test.Equal(t, stdout.String(), "prompt showy showy\n")
+	test.Equal(t, stdout.String(), fmt.Sprintf("\n~/project\n%s", module.Symbol()))
 }
